9b30d3c3c5ced5133e146d685ace48e9fdcad629
rjaybernardo <raphaeljaybernardo@gmail.com>
2023-05-27 21:59:34 +0800

    change title

--- components/Header.js
+++ components/Header.js
@@ -1,32 +1,35 @@
-import Link from "next/link";
-import styled from "styled-components";
-import Center from "@/components/Center";
-import {useContext, useState} from "react";
-import {CartContext} from "@/components/CartContext";
-import BarsIcon from "@/components/icons/Bars";
-import SearchIcon from "@/components/icons/SearchIcon";
+import Link from 'next/link'
+import styled from 'styled-components'
+import Center from '@/components/Center'
+import { useContext, useState } from 'react'
+import { CartContext } from '@/components/CartContext'
+import BarsIcon from '@/components/icons/Bars'
+import SearchIcon from '@/components/icons/SearchIcon'
 
 const StyledHeader = styled.header`
   background-color: #222;
-  position:sticky;
-  top:0;
-  z-index:10;
-`;
+  position: sticky;
+  top: 0;
+  z-index: 10;
+`
 const Logo = styled(Link)`
-  color:#fff;
-  text-decoration:none;
+  color: #fff;
+  text-decoration: none;
   position: relative;
   z-index: 3;
-`;
+`
 const Wrapper = styled.div`
   display: flex;
   justify-content: space-between;
   padding: 20px 0;
-`;
+`
 const StyledNav = styled.nav`
-  ${props => props.mobileNavActive ? `
+  ${(props) =>
+    props.mobileNavActive
+      ? `
     display: block;
-  ` : `
+  `
+      : `
     display: none;
   `}
   gap: 15px;
@@ -42,25 +45,25 @@ const StyledNav = styled.nav`
     position: static;
     padding: 0;
   }
-`;
+`
 const NavLink = styled(Link)`
   display: block;
-  color:#aaa;
-  text-decoration:none;
-  min-width:30px;
+  color: #aaa;
+  text-decoration: none;
+  min-width: 30px;
   padding: 10px 0;
-  svg{
-    height:20px;
+  svg {
+    height: 20px;
   }
   @media screen and (min-width: 768px) {
-    padding:0;
+    padding: 0;
   }
-`;
+`
 const NavButton = styled.button`
   background-color: transparent;
   width: 30px;
   height: 30px;
-  border:0;
+  border: 0;
   color: white;
   cursor: pointer;
   position: relative;
@@ -68,29 +71,29 @@ const NavButton = styled.button`
   @media screen and (min-width: 768px) {
     display: none;
   }
-`;
+`
 const SideIcons = styled.div`
   display: flex;
   align-items: center;
-  a{
-    display:inline-block;
-    min-width:20px;
-    color:white;
-    svg{
-      width:14px;
-      height:14px;
+  a {
+    display: inline-block;
+    min-width: 20px;
+    color: white;
+    svg {
+      width: 14px;
+      height: 14px;
     }
   }
-`;
+`
 
 export default function Header() {
-  const {cartProducts} = useContext(CartContext);
-  const [mobileNavActive,setMobileNavActive] = useState(false);
+  const { cartProducts } = useContext(CartContext)
+  const [mobileNavActive, setMobileNavActive] = useState(false)
   return (
     <StyledHeader>
       <Center>
         <Wrapper>
-          <Logo href={'/'}>Ecommerce</Logo>
+          <Logo href={'/'}>RGB LeatherCrafts</Logo>
           <StyledNav mobileNavActive={mobileNavActive}>
             <NavLink href={'/'}>Home</NavLink>
             <NavLink href={'/products'}>All products</NavLink>
@@ -99,13 +102,15 @@ export default function Header() {
             <NavLink href={'/cart'}>Cart ({cartProducts.length})</NavLink>
           </StyledNav>
           <SideIcons>
-            <Link href={'/search'}><SearchIcon /></Link>
-            <NavButton onClick={() => setMobileNavActive(prev => !prev)}>
+            <Link href={'/search'}>
+              <SearchIcon />
+            </Link>
+            <NavButton onClick={() => setMobileNavActive((prev) => !prev)}>
               <BarsIcon />
             </NavButton>
           </SideIcons>
         </Wrapper>
       </Center>
     </StyledHeader>
-  );
+  )
 }
