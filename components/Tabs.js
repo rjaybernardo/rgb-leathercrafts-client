4da0f9de52312156e6ccc139dd25f88dc60a83c1
rjaybernardo <raphaeljaybernardo@gmail.com>
2023-05-29 05:26:20 +0800

    fix missing key

--- components/Tabs.js
+++ components/Tabs.js
@@ -1,30 +1,38 @@
-import styled from "styled-components";
+import styled from 'styled-components'
 
 const StyledTabs = styled.div`
   display: flex;
   gap: 20px;
   margin-bottom: 20px;
-`;
+`
 const StyledTab = styled.span`
   font-size: 1.5rem;
   cursor: pointer;
-  ${props => props.active ? `
+  ${(props) =>
+    props.active
+      ? `
     color:black;
     border-bottom: 2px solid black;
-  ` : `
+  `
+      : `
     color:#999;
   `}
-`;
+`
 
-export default function Tabs({tabs,active,onChange}) {
+export default function Tabs({ tabs, active, onChange }) {
   return (
     <StyledTabs>
-      {tabs.map(tabName => (
+      {tabs.map((tabName) => (
         <StyledTab
-          onClick={() => { onChange(tabName) }}
+          onClick={() => {
+            onChange(tabName)
+          }}
           active={tabName === active}
-        >{tabName}</StyledTab>
+          key={tabName._id}
+        >
+          {tabName}
+        </StyledTab>
       ))}
     </StyledTabs>
-  );
+  )
 }
